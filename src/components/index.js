c05b05881ea9e0510a64e522adadd4066c269fae
Davit Vacheishvili <75946390+vache02@users.noreply.github.com>
2023-07-06 16:14:16 +0400

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Business from "./Business";
+import Billing from "./Billing";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedBackCard from "./FeedBackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export{
+  Navbar,
+  Business,
+  Billing,
+  Button,
+  CardDeal,
+  Clients,
+  CTA,
+  FeedBackCard,
+  Footer,
+  GetStarted,
+  Hero,
+  Stats,
+  Testimonials
+}
